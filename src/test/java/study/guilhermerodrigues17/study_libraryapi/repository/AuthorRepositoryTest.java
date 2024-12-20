a5da1cf5bd9a006a3f63764d486abbae902ee774
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-20 18:43:31 -0300

    :test_tube: feat(test/AuthorRepository): Implementados testes para insert e update de Author na database

--- src/test/java/study/guilhermerodrigues17/study_libraryapi/repository/AuthorRepositoryTest.java
+++ src/test/java/study/guilhermerodrigues17/study_libraryapi/repository/AuthorRepositoryTest.java
@@ -1,8 +1,44 @@
 package study.guilhermerodrigues17.study_libraryapi.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.guilhermerodrigues17.study_libraryapi.model.Author;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 public class AuthorRepositoryTest {
 
+    @Autowired
+    AuthorRepository repository;
+
+    @Test
+    public void saveAuthorTest() {
+        Author author = new Author();
+        author.setName("José");
+        author.setNationality("Brazilian");
+        author.setBirthDate(LocalDate.of(1950, 1, 4));
+
+        Author savedAuthor = repository.save(author);
+        System.out.println("Saved author: " + savedAuthor);
+    }
+
+    @Test
+    public void updateAuthorTest() {
+        var id = UUID.fromString("aa0d1ec5-389f-48b9-9d28-de1f82219db7");
+
+        Optional<Author> optionalAuthor = repository.findById(id);
+
+        if (optionalAuthor.isPresent()) {
+            Author authorFound = optionalAuthor.get();
+            System.out.println("Author found: " + authorFound);
+
+            authorFound.setBirthDate(LocalDate.of(1960, 1, 4));
+
+            repository.save(authorFound);
+        }
+    }
 }
