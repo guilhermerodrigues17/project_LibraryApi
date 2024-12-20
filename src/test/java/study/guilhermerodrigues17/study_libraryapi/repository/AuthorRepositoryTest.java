4de8b1ff1a16e9688e9156fdd9d40d12c3590778
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-20 18:13:27 -0300

    :test_tube: feat(test/AuthorRepository): Criada classe de testes no repository de Author

---
+++ src/test/java/study/guilhermerodrigues17/study_libraryapi/repository/AuthorRepositoryTest.java
@@ -0,0 +1,8 @@
+package study.guilhermerodrigues17.study_libraryapi.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthorRepositoryTest {
+
+}
