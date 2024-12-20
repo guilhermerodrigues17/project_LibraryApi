b98f1dc0c134bb0b73f1284f56acd0742f88fe5b
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-20 17:13:41 -0300

    :sparkles: feat(model/Book): Implementado e mapeado a entidade Book e relacionamento com Author

---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/model/Book.java
@@ -0,0 +1,37 @@
+package study.guilhermerodrigues17.study_libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book", schema = "public")
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    private String isbn;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(name = "publicationdate", nullable = false)
+    private LocalDate publicationDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private BookGenres genre;
+
+    @Column(precision = 18, scale = 2)
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_author")
+    private Author author;
+}
---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/model/BookGenres.java
@@ -0,0 +1,10 @@
+package study.guilhermerodrigues17.study_libraryapi.model;
+
+public enum BookGenres {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    BIOGRAFIA,
+    CIENCIA
+}
