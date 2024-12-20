87ffed4fcbd05e6cba90f69b31a74c1746fcad05
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-20 16:29:35 -0300

    :sparkles: feat(model/Author): Implementado e mapeado a entidade Author

---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/model/Author.java
@@ -0,0 +1,28 @@
+package study.guilhermerodrigues17.study_libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, name = "birthdate")
+    private LocalDate birthDate;
+
+    @Column(nullable = false, length = 50)
+    private String nationality;
+}
