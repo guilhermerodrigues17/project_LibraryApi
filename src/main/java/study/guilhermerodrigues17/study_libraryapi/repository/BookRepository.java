72ed1b36d71f4f736fe33ba2fab3fd7c3e253368
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-26 18:30:44 -0300

    :sparkles: feat(repository/BookRepository): Implementado query method para buscar lista de books por Author

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package study.guilhermerodrigues17.study_libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import study.guilhermerodrigues17.study_libraryapi.model.Author;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    //select * from book where id_author = id
+    List<Book> findByAuthor(Author author);
 }
