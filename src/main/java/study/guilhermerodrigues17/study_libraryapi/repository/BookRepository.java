48a9fcf1ab87d99a7632ba89b45fc136b947ac64
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-27 17:46:45 -0300

    :sparkles: feat(BookRepository): Implementadas queries com operações de update e delete

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package study.guilhermerodrigues17.study_libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.model.BookGenres;
@@ -57,4 +59,14 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
     //Positional parameters
     @Query("select b from Book b where b.genre = ?1 order by ?2")
     List<Book> findByGenrePositionalParams(BookGenres genre, String orderParam);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Book where genre = ?1")
+    void deleteByGenre(BookGenres genre);
+
+    @Modifying
+    @Transactional
+    @Query("update Book set publicationDate = ?2 where id = ?1 ")
+    void updatePublicationDate(UUID id, LocalDate newDate);
 }
