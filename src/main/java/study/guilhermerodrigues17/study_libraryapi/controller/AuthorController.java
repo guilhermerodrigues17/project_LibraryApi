c48096c537461ec79726cb140155747b12d7fdad
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-06 15:19:06 -0300

    :sparkles: feat(AuthorController): Implementado endpoint para findById de Author

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/AuthorController.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/AuthorController.java
@@ -2,16 +2,15 @@ package study.guilhermerodrigues17.study_libraryapi.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import study.guilhermerodrigues17.study_libraryapi.controller.dto.AuthorDTO;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 import study.guilhermerodrigues17.study_libraryapi.service.AuthorService;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("authors") //http://localhost:8080/authors
@@ -33,5 +32,23 @@ public class AuthorController {
                 .buildAndExpand(authorEntity.getId()).toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AuthorDTO> findById(@PathVariable String id) {
+        UUID uuid = UUID.fromString(id);
+        Optional<Author> optionalAuthor = service.findById(uuid);
+        if (optionalAuthor.isPresent()) {
+            Author author = optionalAuthor.get();
+            AuthorDTO dto = new AuthorDTO(
+                    author.getId(),
+                    author.getName(),
+                    author.getBirthDate(),
+                    author.getNationality()
+            );
+
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
