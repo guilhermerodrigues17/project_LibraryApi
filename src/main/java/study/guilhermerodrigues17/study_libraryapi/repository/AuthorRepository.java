66dacedd191f1f9738c845a984958ca950b41f11
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-06 17:13:33 -0300

    :sparkles: feat(AuthorRepository): Implementados métodos personalizados de pesquisa para Author

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/AuthorRepository.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/AuthorRepository.java
@@ -3,7 +3,12 @@ package study.guilhermerodrigues17.study_libraryapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    List<Author> findByName(String name);
+    List<Author> findByNationality(String nationality);
+    List<Author> findByNameAndNationality(String name, String nationality);
 }
