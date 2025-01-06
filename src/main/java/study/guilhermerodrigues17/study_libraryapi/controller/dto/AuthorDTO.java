7139a70ddc6351bc3770cf0f40bcf4a588a6d89e
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-06 15:18:01 -0300

    :sparkles: feat(dto): Inserido id no construtor do dto de Author

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
@@ -3,8 +3,9 @@ package study.guilhermerodrigues17.study_libraryapi.controller.dto;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record AuthorDTO(String name, LocalDate birthDate, String nationality) {
+public record AuthorDTO(UUID id, String name, LocalDate birthDate, String nationality) {
 
     public Author mapToAuthor() {
         Author author = new Author();
