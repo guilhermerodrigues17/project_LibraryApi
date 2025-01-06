4e95550816f48145f36a36faec17cc626c0a6e39
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-06 17:14:22 -0300

    :sparkles: feat(AuthorService): Implementado método de pesquisa de Author por nome e nacionalidade

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/service/AuthorService.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/service/AuthorService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 import study.guilhermerodrigues17.study_libraryapi.repository.AuthorRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,5 +27,21 @@ public class AuthorService {
 
     public void deleteAuthor(Author author) {
         repository.delete(author);
+    }
+
+    public List<Author> searchAuthor(String name, String nationality) {
+        if (name != null && nationality != null) {
+            return repository.findByNameAndNationality(name, nationality);
+        }
+
+        if (name != null) {
+            return repository.findByName(name);
+        }
+
+        if (nationality != null) {
+            return repository.findByNationality(nationality);
+        }
+
+        return repository.findAll();
     }
 }
