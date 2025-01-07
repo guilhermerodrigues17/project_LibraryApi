c21609b06162b2650f399452127c5a94e1cbc30a
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-07 16:56:57 -0300

    :sparkles: feat(AuthorDTO): Implementadas annotations de validation para Author

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
@@ -1,11 +1,21 @@
 package study.guilhermerodrigues17.study_libraryapi.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record AuthorDTO(UUID id, String name, LocalDate birthDate, String nationality) {
+public record AuthorDTO(
+        UUID id,
+        @NotBlank(message = "Required field")
+        String name,
+        @NotNull(message = "Required field")
+        LocalDate birthDate,
+        @NotBlank(message = "Required field")
+        String nationality
+) {
 
     public Author mapToAuthor() {
         Author author = new Author();
