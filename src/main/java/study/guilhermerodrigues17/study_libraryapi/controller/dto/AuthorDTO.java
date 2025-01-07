0caf511af3d589a6ac5b14633d0a3c9687fbf3da
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-07 17:30:41 -0300

    :sparkles: feat(AuthorDTO): Implementadas novas validações de 'size' para strings e 'past' em birthDate

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/AuthorDTO.java
@@ -2,6 +2,8 @@ package study.guilhermerodrigues17.study_libraryapi.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import study.guilhermerodrigues17.study_libraryapi.model.Author;
 
 import java.time.LocalDate;
@@ -10,10 +12,13 @@ import java.util.UUID;
 public record AuthorDTO(
         UUID id,
         @NotBlank(message = "Required field")
+        @Size(min = 2, max = 100, message = "Invalid field size")
         String name,
         @NotNull(message = "Required field")
+        @Past(message = "Invalid date. It cant be in the future")
         LocalDate birthDate,
         @NotBlank(message = "Required field")
+        @Size(min = 2, max = 50, message = "Invalid field size")
         String nationality
 ) {
 
