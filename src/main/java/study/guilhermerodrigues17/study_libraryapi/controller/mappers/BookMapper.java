5fdc0dcf13a1b4c187ee674778257183e780d78e
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-08 19:08:06 -0300

    :sparkles: feat(mapper): Criado mapper para a entidade Book

---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/mappers/BookMapper.java
@@ -0,0 +1,18 @@
+package study.guilhermerodrigues17.study_libraryapi.controller.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import study.guilhermerodrigues17.study_libraryapi.controller.dto.BookRequestDTO;
+import study.guilhermerodrigues17.study_libraryapi.model.Book;
+import study.guilhermerodrigues17.study_libraryapi.repository.AuthorRepository;
+
+@Mapper(componentModel = "spring")
+public abstract class BookMapper {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @Mapping(target = "author", expression = "java( authorRepository.findById(dto.authorId()).orElse(null) )")
+    public abstract Book toEntity(BookRequestDTO dto);
+}
