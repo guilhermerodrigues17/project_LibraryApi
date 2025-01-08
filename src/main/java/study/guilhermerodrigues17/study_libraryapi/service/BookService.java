a56e7e18608df7055c43e5281f7acba8352d51f4
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-08 19:09:08 -0300

    :sparkles: feat(BookService): Implementado método save

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
@@ -2,6 +2,7 @@ package study.guilhermerodrigues17.study_libraryapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.repository.BookRepository;
 
 @Service
@@ -9,4 +10,8 @@ import study.guilhermerodrigues17.study_libraryapi.repository.BookRepository;
 public class BookService {
 
     private final BookRepository repository;
+
+    public Book save(Book book) {
+        return repository.save(book);
+    }
 }
