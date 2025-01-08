527896c8a6b2e94efffb0661edd1c10e20af219e
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-08 17:35:31 -0300

    :sparkles: feat(dto): Implementadas validações de campos obrigatórios em BookRequestDTO

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/BookRequestDTO.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/dto/BookRequestDTO.java
@@ -1,5 +1,9 @@
 package study.guilhermerodrigues17.study_libraryapi.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 import study.guilhermerodrigues17.study_libraryapi.model.BookGenres;
 
 import java.math.BigDecimal;
@@ -7,11 +11,17 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookRequestDTO(
+        @ISBN
+        @NotBlank(message = "Required Field")
         String isbn,
+        @NotBlank(message = "Required Field")
         String title,
+        @NotNull(message = "Required Field")
+        @Past(message = "Invalid date. It cant be in the future")
         LocalDate publicationDate,
         BookGenres genre,
         BigDecimal price,
+        @NotNull(message = "Required Field")
         UUID authorId
 ) {
 
