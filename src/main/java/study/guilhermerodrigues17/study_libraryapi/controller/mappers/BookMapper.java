d2f11e19539a641d56e59d0dd63061079d810498
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 12:35:03 -0300

    :sparkles: feat(BookMapper): Implementado método toDTO()

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/mappers/BookMapper.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/mappers/BookMapper.java
@@ -4,10 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import study.guilhermerodrigues17.study_libraryapi.controller.dto.BookRequestDTO;
+import study.guilhermerodrigues17.study_libraryapi.controller.dto.BookResponseDTO;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.repository.AuthorRepository;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = AuthorMapper.class)
 public abstract class BookMapper {
 
     @Autowired
@@ -15,4 +16,6 @@ public abstract class BookMapper {
 
     @Mapping(target = "author", expression = "java( authorRepository.findById(dto.authorId()).orElse(null) )")
     public abstract Book toEntity(BookRequestDTO dto);
+
+    public abstract BookResponseDTO toDTO(Book book);
 }
