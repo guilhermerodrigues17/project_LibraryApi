a0ba0de2b4d92098ba81082cf3d67c5e914c9bda
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 17:37:53 -0300

    :sparkles: feat(BookRepository): Repositório agora extende de JpaSpecificationExecutor também

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package study.guilhermerodrigues17.study_libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
 
     //select * from book where id_author = id
     List<Book> findByAuthor(Author author);
