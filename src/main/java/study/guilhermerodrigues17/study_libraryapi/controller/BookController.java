16b499a124a7a3820fdf1bbd893dbd8eab492dc7
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 12:36:33 -0300

    :sparkles: feat(BookController): Implementado endpoint get para o método de consulta findById()

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/BookController.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/BookController.java
@@ -3,16 +3,15 @@ package study.guilhermerodrigues17.study_libraryapi.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.guilhermerodrigues17.study_libraryapi.controller.dto.BookRequestDTO;
+import study.guilhermerodrigues17.study_libraryapi.controller.dto.BookResponseDTO;
 import study.guilhermerodrigues17.study_libraryapi.controller.mappers.BookMapper;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.service.BookService;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("books")
@@ -28,5 +27,13 @@ public class BookController implements GenericController {
         service.save(bookEntity);
         URI uri = generateHeaderLocation(bookEntity.getId());
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<BookResponseDTO> findById(@PathVariable String id) {
+        return service.findById(UUID.fromString(id)).map(book -> {
+            var dto = mapper.toDTO(book);
+            return ResponseEntity.ok(dto);
+        }).orElseGet( () -> ResponseEntity.notFound().build() );
     }
 }
