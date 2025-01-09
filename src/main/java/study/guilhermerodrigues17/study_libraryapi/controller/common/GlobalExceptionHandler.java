f1fedc23e6d4d99e2ae0525c5ce6c703d6f5a338
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 12:05:44 -0300

    :sparkles: feat(GlobalExceptionHandler): Implementada captura para conflict, bad request e internal error no handler global

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/common/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import study.guilhermerodrigues17.study_libraryapi.controller.dto.ExceptionFields;
 import study.guilhermerodrigues17.study_libraryapi.controller.dto.ExceptionResponse;
+import study.guilhermerodrigues17.study_libraryapi.exceptions.DuplicatedRegisterException;
+import study.guilhermerodrigues17.study_libraryapi.exceptions.NotAllowedOperationException;
 
 import java.util.List;
 
@@ -28,6 +30,28 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
                 "Validation error!",
                 exceptionFields
+        );
+    }
+
+    @ExceptionHandler(DuplicatedRegisterException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ExceptionResponse handleDuplicatedRegisterException(DuplicatedRegisterException e) {
+        return ExceptionResponse.conflictResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NotAllowedOperationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleNotAllowedOperationException(NotAllowedOperationException e) {
+        return ExceptionResponse.defaultResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleInternalServerException(RuntimeException e) {
+        return new ExceptionResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred!",
+                List.of()
         );
     }
 }
