0df0abded3ec0c67c6b927d77ed31fbac7256dc5
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 17:39:54 -0300

    :sparkles: feat(BookService): Implementado método searchBooksBySpecs()

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
@@ -1,10 +1,15 @@
 package study.guilhermerodrigues17.study_libraryapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
+import study.guilhermerodrigues17.study_libraryapi.model.BookGenres;
 import study.guilhermerodrigues17.study_libraryapi.repository.BookRepository;
 
+import static study.guilhermerodrigues17.study_libraryapi.repository.specs.BookSpecs.*;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,5 +29,21 @@ public class BookService {
 
     public void deleteById(Book book) {
         repository.delete(book);
+    }
+
+    public List<Book> searchBooksBySpecs(String isbn, String title, String authorName,
+                                         BookGenres genre, Integer publicationYear) {
+        Specification<Book> specs = Specification.where(
+                (root, query, criteriaBuilder) ->
+                        criteriaBuilder.conjunction()
+        );
+
+        if (isbn != null) specs = specs.and(isbnEqual(isbn));
+        if (title != null) specs = specs.and(titleLike(title));
+        if (genre != null) specs = specs.and(genreEqual(genre));
+        if (publicationYear != null) specs = specs.and(publicationYearEqual(publicationYear));
+        if (authorName != null) specs = specs.and(authorNameLike(authorName));
+
+        return repository.findAll(specs);
     }
 }
