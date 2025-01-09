045abe7d604c97dcf822b1f1a99ab05e158d6309
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-09 12:35:37 -0300

    :sparkles: feat(BookService): Implementado método findById() para consultas

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/service/BookService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.repository.BookRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
@@ -13,5 +16,9 @@ public class BookService {
 
     public Book save(Book book) {
         return repository.save(book);
+    }
+
+    public Optional<Book> findById(UUID id) {
+        return repository.findById(id);
     }
 }
