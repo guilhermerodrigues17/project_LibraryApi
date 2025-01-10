eca98bb6a838684567b9afb4814390d98f1ed21c
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-10 20:01:34 -0300

    :sparkles: feat(BookValidator): Implementada validação para regra de negócio do preço

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/validator/BookValidator.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/validator/BookValidator.java
@@ -3,6 +3,7 @@ package study.guilhermerodrigues17.study_libraryapi.validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import study.guilhermerodrigues17.study_libraryapi.exceptions.DuplicatedRegisterException;
+import study.guilhermerodrigues17.study_libraryapi.exceptions.InvalidFieldException;
 import study.guilhermerodrigues17.study_libraryapi.model.Book;
 import study.guilhermerodrigues17.study_libraryapi.repository.BookRepository;
 
@@ -12,12 +13,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BookValidator {
 
+    private static final int PUBLICATION_YEAR = 2020;
+
     private final BookRepository repository;
 
     public void validate(Book book) {
         if (isbnAlreadyExists(book)) {
             throw new DuplicatedRegisterException("ISBN already exists!");
         }
+
+        if (isMandatoryPriceNull(book)) {
+            throw new InvalidFieldException("Price is mandatory for books released after 2020!", "Price");
+        }
+    }
+
+    private boolean isMandatoryPriceNull(Book book) {
+        return book.getPrice() == null && book.getPublicationDate().getYear() >= PUBLICATION_YEAR;
     }
 
     public boolean isbnAlreadyExists(Book book) {
