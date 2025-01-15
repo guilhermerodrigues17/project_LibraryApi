9450aa9f725e3584a6ab4f84ab9b44902dddca90
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-15 17:02:40 -0300

    :sparkles: feat(repository): Implementado repository de User

---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package study.guilhermerodrigues17.study_libraryapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.guilhermerodrigues17.study_libraryapi.model.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}
