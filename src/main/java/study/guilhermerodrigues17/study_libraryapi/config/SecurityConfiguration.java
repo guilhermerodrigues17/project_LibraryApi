b3b7222874bb31a8f9d7e665ce706ae7c1d18ef4
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-15 17:36:47 -0300

    :sparkles: feat(SecurityConfiguration): Adotando implementação CustomUserDetailsService para contexto de security

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
@@ -6,13 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import study.guilhermerodrigues17.study_libraryapi.security.CustomUserDetailsService;
+import study.guilhermerodrigues17.study_libraryapi.service.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -41,19 +40,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails user1 = User.builder()
-                .username("user")
-                .password(encoder.encode("123"))
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password(encoder.encode("321"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
     }
 }
