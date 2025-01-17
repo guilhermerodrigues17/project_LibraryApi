a9456e4cbb1dc73ba5b60e791fb255c172294b1b
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-17 20:14:19 -0300

    :sparkles: feat(SecurityConfiguration): Eliminando rolePrefix do contexto do security

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,8 +42,13 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
+//    @Bean
     public UserDetailsService userDetailsService(UserService userService) {
         return new CustomUserDetailsService(userService);
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
     }
 }
