e6613250648af57afe3fb5ccd1973dfd0aef5bb2
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-23 16:31:37 -0300

    :sparkles: feat(security): Implementada repository de RegisteredClient

---
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,49 @@
+package study.guilhermerodrigues17.study_libraryapi.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Component;
+import study.guilhermerodrigues17.study_libraryapi.model.Client;
+import study.guilhermerodrigues17.study_libraryapi.service.ClientService;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        Client client = clientService.findByClientId(clientId);
+
+        if (client == null) return null;
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectUri())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
+                .build();
+    }
+}
