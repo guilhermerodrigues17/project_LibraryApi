67e396af7802ec0db7e7a212761979cd55f5166a
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-23 17:17:32 -0300

    :sparkles: feat(AuthorizationServerConfig): Implementado bean de SecurityFilterChain para o authServer

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/AuthorizationServerConfiguration.java
@@ -2,18 +2,42 @@ package study.guilhermerodrigues17.study_libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfigurer authServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        http.securityMatcher(authServerConfigurer.getEndpointsMatcher())
+                .with(authServerConfigurer, (authServer) -> {
+                    authServer.oidc(Customizer.withDefaults());
+                }
+        );
+
+        http.oauth2ResourceServer(oAuth2Rs -> oAuth2Rs.jwt(Customizer.withDefaults()));
+
+        http.formLogin(configurer -> configurer.loginPage("/login"));
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
