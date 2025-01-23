d597b4370af7a2df95f4e80ee329fee603707453
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-23 16:42:17 -0300

    :sparkles: feat(AuthorizationServerConfig): Implementado bean de PasswordEncoder

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/AuthorizationServerConfiguration.java
@@ -3,6 +3,8 @@ package study.guilhermerodrigues17.study_libraryapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
@@ -12,6 +14,11 @@ import java.time.Duration;
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
     @Bean
     public TokenSettings tokenSettings() {
