0a96531fc48c208bac99cd4007ce50adaa349966
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-23 16:40:10 -0300

    :sparkles: style(SecurityConfiguration): Removidos imports não utilizados

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/SecurityConfiguration.java
@@ -8,13 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import study.guilhermerodrigues17.study_libraryapi.security.CustomUserDetailsService;
 import study.guilhermerodrigues17.study_libraryapi.security.SocialLoginSuccessHandler;
-import study.guilhermerodrigues17.study_libraryapi.service.UserService;
 
 @Configuration
 @EnableWebSecurity
