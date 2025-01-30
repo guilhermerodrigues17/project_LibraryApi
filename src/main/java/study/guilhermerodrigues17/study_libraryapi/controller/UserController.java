6be5b3e9fdf035e643ad7035661dd4ec9d0f5089
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-30 15:53:33 -0300

    :sparkles: feat(Swagger/UserController): Alterado título e mapeada operação e response

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/UserController.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/UserController.java
@@ -1,5 +1,9 @@
 package study.guilhermerodrigues17.study_libraryapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +15,7 @@ import study.guilhermerodrigues17.study_libraryapi.service.UserService;
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
+@Tag(name = "Users")
 public class UserController {
 
     private final UserService service;
@@ -18,6 +23,11 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Save", description = "Save a new user.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Saved successful."),
+            @ApiResponse(responseCode = "422", description = "Validation error.")
+    })
     public void save(@RequestBody @Valid UserDTO dto) {
         var user = mapper.toEntity(dto);
         service.save(user);
