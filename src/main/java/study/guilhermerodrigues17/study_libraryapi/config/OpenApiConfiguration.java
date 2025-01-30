c022138c5fa506f164be104570cee53e1d5994bc
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-30 16:29:37 -0300

    :sparkles: feat(Swagger/SecuritySchema): Habilitada autenticação OAUTH2 na documentação

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package study.guilhermerodrigues17.study_libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,7 +19,17 @@ import org.springframework.context.annotation.Configuration;
                         email = "guilhermerodriguesm23@gmail.com",
                         url = "libraryapi.com"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }
