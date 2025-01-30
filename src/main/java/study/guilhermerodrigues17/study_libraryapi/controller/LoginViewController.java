fca170c0271dfe3523401bd8ada28f4d071c7630
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-01-30 15:49:11 -0300

    :sparkles: feat(Swagger/LoginViewController): Alterado título e mapeadas operações e responses

--- src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/LoginViewController.java
+++ src/main/java/study/guilhermerodrigues17/study_libraryapi/controller/LoginViewController.java
@@ -1,5 +1,9 @@
 package study.guilhermerodrigues17.study_libraryapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +12,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import study.guilhermerodrigues17.study_libraryapi.security.CustomAuthentication;
 
 @Controller
+@Tag(name = "Login")
 public class LoginViewController {
 
     @GetMapping("/login")
+    @Operation(summary = "Find Login View", description = "Return a login view.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success.")
+    })
     public String findLoginView() {
         return "login";
     }
 
     @GetMapping("/")
     @ResponseBody
+    @Operation(summary = "Find Home View", description = "Return a home view.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success.")
+    })
     public String findHomeView(Authentication authentication) {
         if (authentication instanceof CustomAuthentication customAuth) {
             System.out.println(customAuth.getUser());
@@ -26,6 +39,10 @@ public class LoginViewController {
 
     @GetMapping("/authorized")
     @ResponseBody
+    @Operation(summary = "Get Code", description = "Get an authorization code.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     public String getCode(@RequestParam String code) {
         return "Generated authentication code: " + code;
     }
